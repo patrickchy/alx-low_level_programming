21af1fc6d41f6cea81d27f1272b5f48f96ce0815
patrickchy <chinyeremakapatrick@gmail.com>
2023-08-12 22:07:17 -0700

    /alx-low_level_programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints all possible different combinations of two digits.
- * Numbers must be separated by ", " and in ascending order.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    int i, j;
-
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
+        int i, j;
+        
+        for (i = 0; i <= 9; i++)
         {
-            if (!(i == 0 && j == 1))
-            {
-                if (i != 0 || j != 1)
+                for (j = i + 1; j <= 9; j++)
                 {
-                    putchar(',');
-                    putchar(' ');
+                        if (!(i == 0 && j == 1))
+                        {
+                                if (i != 0 || j != 1)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                                putchar(i + '0');
+                                putchar(j + '0');
+                        }
                 }
-                putchar(i + '0');
-                putchar(j + '0');
-            }
         }
-    }
-
-    putchar('\n');
-
-    return (0);
+        
+        putchar('\n');
+        
+        return (0);
 }
