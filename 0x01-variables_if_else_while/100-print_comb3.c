08e47ac480cd8a6245864de13f152999acef8e17
patrickchy <chinyeremakapatrick@gmail.com>
2023-08-13 01:16:44 -0700

    /alx-low_level_programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - print two digits separated with , and space
+ *
+ * Return: (0)
+ *
+ */
 
 int main(void)
 {
@@ -6,7 +12,7 @@ int main(void)
 
     for (i = 0; i < 10; i++)
     {
-        for (j = i + 1; j <= 10; j++)
+        for (j = i + 1; j < 10; j++)
         {
 		putchar(i + '0');
 		putchar(j + '0');
